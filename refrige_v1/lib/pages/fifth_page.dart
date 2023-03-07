82d9f86f36164a03478f5301375a2c4ae25c8da3
Jihun Choi <98497929+Choijh03@users.noreply.github.com>
2023-03-07 10:38:27 -0800

    Food recommendation roulette 90% done
    just need to update list of the food, all the functionalities are working good

--- refrige_v1/lib/pages/fifth_page.dart
+++ refrige_v1/lib/pages/fifth_page.dart
@@ -1,7 +1,47 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class FifthPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
+
+class FifthPage extends StatefulWidget {
   const FifthPage({super.key});
+
+  @override
+  State<FifthPage> createState() => _FifthPageState();
+}
+
+class _FifthPageState extends State<FifthPage> {
+  int selected = 0;
+  List<String> foods = [
+    'Domino Pizza',
+    'Papa Johns Pizza',
+    'In and Out Burger',
+    'Korean Tufu soup',
+    'Chipotle Burrito',
+    'Kimchi',
+    'Chocolate',
+    'Beef',
+    'Instant Noodle'
+  ];
+  String food = '';
+
+  //choose 5 food from array
+  String food1 = 'food item1';
+  String food2 = 'food item2';
+  String food3 = 'food item3';
+  String food4 = 'food item4';
+  String food5 = 'food item5';
+
+  //randomize the array and choose first five items
+  void randomFood(List<String> list) {
+    list.shuffle();
+
+    food1 = list[0];
+    food2 = list[1];
+    food3 = list[2];
+    food4 = list[3];
+    food5 = list[4];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +51,124 @@ class FifthPage extends StatelessWidget {
         title: Center(child: Text('Food Recommendation Roullete')),
         backgroundColor: Colors.grey,
         elevation: 0, //removing shadow
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            food,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              height: 350,
+              child: FortuneWheel(
+                animateFirst: false,
+                selected: selected,
+                physics: CircularPanPhysics(
+                  duration: Duration(seconds: 1),
+                  curve: Curves.decelerate,
+                ),
+                onFling: () {
+                  setState(() {
+                    selected = Random().nextInt(4);
+                  });
+                },
+                styleStrategy: UniformStyleStrategy(
+                  borderColor: Colors.white,
+                  color: Colors.grey,
+                  borderWidth: 5,
+                ),
+                items: [
+                  FortuneItem(
+                      child: Text(
+                    food1,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )),
+                  FortuneItem(
+                      child: Text(
+                    food2,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )),
+                  FortuneItem(
+                      child: Text(
+                    food3,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )),
+                  FortuneItem(
+                      child: Text(
+                    food4,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )),
+                  FortuneItem(
+                      child: Text(
+                    food5,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )),
+                ],
+                onAnimationEnd: () {
+                  setState(() {
+                    if (selected == 0) {
+                      food = food1;
+                    } else if (selected == 1) {
+                      food = food2;
+                    } else if (selected == 2) {
+                      food = food3;
+                    } else if (selected == 3) {
+                      food = food4;
+                    } else if (selected == 4) {
+                      food = food5;
+                    }
+                  });
+                },
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: MaterialButton(
+              color: Colors.grey[400],
+              onPressed: () {
+                setState(() {
+                  randomFood(foods);
+                });
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text('Click to randomize the list!'),
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                selected = Random().nextInt(4);
+              });
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(
+                top: 16,
+                bottom: 16,
+                left: 25,
+                right: 25,
+              ),
+              child: Container(
+                color: Colors.grey[400],
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      'Click to Spin!',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
